3bed1824430a79ca0890d1bdc1c6fa4bc5ad73ee
Kyra <95829016+OneMoreKyra@users.noreply.github.com>
2024-03-16 19:49:49 -0600

    Update‎ version.py

---
+++ ‎ version.py
@@ -0,0 +1,5 @@
+def main(usuario_id):
+    print("Version 0.3 actualizado el 16/03/24 c:")
+
+if __name__ == "__main__":
+    main()
--- ㅤversion.py
+++
@@ -1,5 +0,0 @@
-def main():
-    print("Version 0.2 actualizado el 16/03/24 c:")
-
-if __name__ == "__main__":
-    main()
