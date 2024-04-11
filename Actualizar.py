5b0de79099394036ad4cc6cdd7b49d8fc7bd250c
Kyra <95829016+OneMoreKyra@users.noreply.github.com>
2024-04-11 17:37:04 -0600

    Update Actualizar.py

--- Actualizar.py
+++ Actualizar.py
@@ -1,28 +1,17 @@
 import requests
 import os
-import sys
 from colorama import Fore, init
 
-def main(exe_name):
+def main():
     init()  # Inicializa colorama
-    url_exe = 'https://github.com/OneMoreKyra/iniciador/raw/main/' + exe_name  # URL del nuevo archivo .exe
+    url_exe = 'https://github.com/OneMoreKyra/iniciador/raw/main/Sniper.exe'
     r = requests.get(url_exe)
-    temp_exe_path = os.path.join(os.getcwd(), 'temp.exe')
-    final_exe_path = os.path.join(os.getcwd(), exe_name)  # Nombre del archivo .exe a reemplazar
+    exe_path = os.path.join(os.getcwd(), 'Sniper.exe')
 
-    print(Fore.YELLOW + "Descargando el archivo...")
-    with open(temp_exe_path, 'wb') as f:
+    print(Fore.YELLOW + "Descargando el archivo 'Sniper.exe'...")
+    with open(exe_path, 'wb') as f:
         f.write(r.content)
-    print(Fore.GREEN + "El archivo ha sido descargado exitosamente.")
-
-    print(Fore.YELLOW + "Reemplazando el archivo existente...")
-    if os.path.exists(final_exe_path):  # Verifica si el archivo existe
-        os.remove(final_exe_path)  # Elimina el archivo .exe existente
-    os.rename(temp_exe_path, final_exe_path)  # Renombra el nuevo archivo .exe
-    print(Fore.GREEN + "El archivo '" + exe_name + "' ha sido actualizado exitosamente.")
+    print(Fore.GREEN + "El archivo 'Sniper.exe' ha sido actualizado exitosamente.")
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1:  # Verifica si se proporcionó un argumento
-        main(sys.argv[1])  # Usa el primer argumento como el nombre del archivo .exe
-    else:
-        print(Fore.RED + "Por favor, proporciona el nombre del archivo .exe como un argumento.")
+    main()
