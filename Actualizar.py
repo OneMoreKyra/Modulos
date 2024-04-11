cd89ec0602c3e4b3f1d508abf556d95a450d40c1
Kyra <95829016+OneMoreKyra@users.noreply.github.com>
2024-04-11 16:48:15 -0600

    Update Actualizar.py

--- Actualizar.py
+++ Actualizar.py
@@ -3,9 +3,9 @@ import requests
 def main(usuario_id):
     url_script = 'https://raw.githubusercontent.com/OneMoreKyra/iniciador/main/iniciador.py'
     r = requests.get(url_script)
-    with open('iniciador.py', 'w', encoding='utf-8') as f:
+    with open('Iniciador.py', 'w', encoding='utf-8') as f:
         f.write(r.text)
-    print("El archivo 'iniciador.py' ha sido actualizado exitosamente.")
+    print("Actualizado exitosamente.")
 
 if __name__ == "__main__":
     main()
