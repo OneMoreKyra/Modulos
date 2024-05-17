63428e793f5aa99e11f563bbb5d80a9a1a3c63ba
Kyra <95829016+OneMoreKyra@users.noreply.github.com>
2024-05-17 15:21:56 -0600

    Update Amigos.py

--- Amigos.py
+++ Amigos.py
@@ -6,12 +6,6 @@ from colorama import Fore, init
 # Inicializar colorama
 init()
 
-def obtener_nombre_para_mostrar(usuario_id):
-    url = f"https://users.roblox.com/v1/users/{usuario_id}"
-    response = requests.get(url)
-    data = json.loads(response.text)
-    return data['displayName']
-
 def obtener_amigos(usuario_id):
     url = f"https://friends.roblox.com/v1/users/{usuario_id}/friends"
     response = requests.get(url)
@@ -19,8 +13,8 @@ def obtener_amigos(usuario_id):
     amigos = data['data']
     lista_amigos = []
     for amigo in amigos:
-        nombre_para_mostrar = obtener_nombre_para_mostrar(amigo['id'])
-        lista_amigos.append(f"{amigo['name']} ({nombre_para_mostrar}) [ID: {amigo['id']}]")
+        # Ya tenemos todos los datos necesarios en la respuesta de la API de amigos
+        lista_amigos.append(f"{amigo['name']} ({amigo['displayName']}) [ID: {amigo['id']}]")
     return lista_amigos
 
 def guardar_amigos(usuario_id, amigos):
@@ -41,9 +35,14 @@ def comparar_amigos(usuario_id, amigos_actuales):
             amigos_anteriores = f.read().splitlines()
     except FileNotFoundError:
         amigos_anteriores = []
-    amigos_nuevos = [amigo for amigo in amigos_actuales if amigo not in amigos_anteriores]
-    amigos_antiguos = [amigo for amigo in amigos_anteriores if amigo not in amigos_actuales]
-    amigos_que_ya_estaban = [amigo for amigo in amigos_actuales if amigo not in amigos_nuevos]
+
+    ids_amigos_actuales = {amigo.split('[ID: ')[-1].rstrip(']') for amigo in amigos_actuales}
+    ids_amigos_anteriores = {amigo.split('[ID: ')[-1].rstrip(']') for amigo in amigos_anteriores}
+
+    amigos_nuevos = [amigo for amigo in amigos_actuales if amigo.split('[ID: ')[-1].rstrip(']') not in ids_amigos_anteriores]
+    amigos_antiguos = [amigo for amigo in amigos_anteriores if amigo.split('[ID: ')[-1].rstrip(']') not in ids_amigos_actuales]
+    amigos_que_ya_estaban = [amigo for amigo in amigos_actuales if amigo.split('[ID: ')[-1].rstrip(']') in ids_amigos_anteriores]
+
     if amigos_nuevos:
         print(Fore.GREEN + "Amigos nuevos:")
         for amigo in amigos_nuevos:
@@ -56,6 +55,7 @@ def comparar_amigos(usuario_id, amigos_actuales):
         print(Fore.RED + "Amigos que ya no están:")
         for amigo in amigos_antiguos:
             print(Fore.RED + f"{amigo}")
+
     amigos_a_guardar = amigos_que_ya_estaban + amigos_nuevos
     guardar_amigos(usuario_id, amigos_a_guardar)
 
@@ -65,3 +65,6 @@ def main(usuario_id):
 
 # Restablecer colorama
 print(Fore.RESET)
+
+# Ejemplo de ejecución del script
+# main('ID_DEL_USUARIO_AQUI')
