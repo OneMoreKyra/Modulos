d972e02da8385273c3944948bd1090cd83e5081c
Kyra <95829016+OneMoreKyra@users.noreply.github.com>
2024-05-31 13:09:27 -0600

    Update Actualizar.py

--- Actualizar.py
+++ Actualizar.py
@@ -1,11 +1,14 @@
 import requests
 import os
+import sys
+import shutil
 from colorama import Fore, init
 
-def main(usuario_id):
+def main():
     init()  # Inicializa colorama
     url_exe = 'https://github.com/OneMoreKyra/iniciador/raw/main/Sniper.exe'  # URL del nuevo archivo .exe
     exe_path = os.path.join(os.getcwd(), 'Sniper.exe')  # Ruta del archivo .exe a descargar
+    current_exe = sys.argv[0]  # Ruta del archivo .exe actual en ejecución
 
     print(Fore.YELLOW + "Descargando el archivo...")
     r = requests.get(url_exe)
@@ -13,7 +16,14 @@ def main(usuario_id):
         f.write(r.content)
     print(Fore.GREEN + "El archivo ha sido descargado exitosamente.")
 
-    print(Fore.CYAN + "Cierra esta versión del sniper y ejecuta la nueva versión la cual se descargó en el mismo directorio que esta versión, puedes borrar esta versión si así lo deseas y quedarte con la nueva únicamente c:")
+    new_exe = os.path.join(os.getcwd(), 'Sniper_new.exe')
+    os.rename(exe_path, new_exe)
+
+    print(Fore.CYAN + "Cerrando la versión actual y ejecutando la nueva versión...")
+
+    # Ejecuta el nuevo archivo y cierra el actual
+    os.startfile(new_exe)
+    sys.exit()
 
 if __name__ == "__main__":
     main()
